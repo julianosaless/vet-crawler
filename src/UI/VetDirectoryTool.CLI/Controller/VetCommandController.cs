8ea37c204f05a8f2be665253d20fdb95cd5e133e
Juliano Sales <juliano.salesg@gmail.com>
2021-02-04 01:54:52 -0300

    UI : Create command line args

---
+++ src/UI/VetDirectoryTool.CLI/Controller/VetCommandController.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+using McMaster.Extensions.CommandLineUtils;
+
+using VetDirectoryTool.Core.Service.Analyzers;
+using VetDirectoryTool.Core.Service.Analyzers.Model;
+
+namespace VetDirectoryTool.CLI.Controller
+{
+    public class VetCommandController : CommandControllerBase
+    {
+        public VetCommandController(CommandLineApplication app) : base(app)
+        {
+        }
+
+        protected override string Name => "directory";
+
+        public override void Apply()
+        {
+            App.Command(Name, (command) =>
+            {
+                command.Description = "";
+                command.ExtendedHelpText = "";
+                command.HelpOption("-?|-h|--help");
+
+                var exportOutput = command.Option(
+                    "-o|--output <OUTPUT_DIR>",
+                    "",
+                    CommandOptionType.SingleValue);
+
+                var verboseOption = command.Option(
+                    "--verbose",
+                    "",
+                    CommandOptionType.NoValue);
+
+                command.OnExecute(async () =>
+                {
+                    var outputPath = exportOutput.HasValue() ? exportOutput.Value() : Directory.GetCurrentDirectory();
+
+                    var directoryModel = new DirectoryModel("https://www.1800petmeds.com/vetdirectory", outputPath, verboseOption.HasValue());
+                    await CreateAnalyzeAsync(directoryModel);
+                    return 0;
+                });
+            });
+        }
+
+        private async Task CreateAnalyzeAsync(DirectoryModel directoryModel)
+        {
+            await AnalyzerService
+                .CreateAnalyzer(path: "https://www.1800petmeds.com/vetdirectory")
+                .AnalyzeAsync(directoryModel);
+        }
+    }
+}
---
+++ src/UI/VetDirectoryTool.CLI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+
+using McMaster.Extensions.CommandLineUtils;
+
+using VetDirectoryTool.CLI.Controller;
+using VetDirectoryTool.Core.Service.Analyzers;
+using VetDirectoryTool.Core.Service.Analyzers.Model;
+
+namespace VetDirectoryTool.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication
+            {
+                Name = "",
+                Description = ""
+            };
+
+            app.HelpOption("-?|-h|--help");
+            app.VersionOption("-v|--version", () => $"Version {Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion}");
+
+            new VetCommandController(app).Apply();
+
+            app.OnExecute(async () =>
+            {
+
+#if DEBUG
+                const string path = @"https://www.1800petmeds.com/vetdirectory";
+                const string exportPath = @"C:\\Temp";
+                var analyzeModel = new DirectoryModel(path, exportPath, verbose: true);
+               
+                var analyzeService = AnalyzerService.CreateAnalyzer(analyzeModel.Path);
+                await analyzeService.AnalyzeAsync(analyzeModel);
+#endif
+                return 0;
+            });
+
+            try
+            {
+                app.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
