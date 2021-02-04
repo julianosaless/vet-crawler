66d6d9a2bfbbc4735667f6720d8bd453a1351119
Juliano Sales <juliano.salesg@gmail.com>
2021-02-04 01:54:08 -0300

    File provider: Create file provider by url

---
+++ src/Core/VetDirectoryTool.Core/Service/FileProvider/RemoteUrlProviderService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace VetDirectoryTool.Core.Service.FileProvider
+{
+    public class RemoteUrlProviderService : IFileProvider
+    {
+        private readonly string Url;
+        private readonly string Output;
+
+        public RemoteUrlProviderService(string url, string output)
+        {
+            Url = url;
+            Output = output;
+        }
+
+        public async Task<string> GetContentAsync()
+        {
+            using WebClient client = new WebClient();
+            await client.DownloadFileTaskAsync(new Uri(Url), Output);
+            return "";
+        }
+    }
+}
