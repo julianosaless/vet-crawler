3f432f56cf72963b568abaffe9cd4f5b1a080a4a
Juliano Sales <juliano.salesg@gmail.com>
2021-02-04 01:53:20 -0300

    Add analyzer structure

---
+++ src/Core/VetDirectoryTool.Core/Service/Analyzers/AnalyzerService.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+using VetDirectoryTool.Core.Service.FileProvider;
+using VetDirectoryTool.Core.Service.Reporting;
+
+namespace VetDirectoryTool.Core.Service.Analyzers
+{
+    public static class AnalyzerService
+    {
+        public static PetMedsAnalyzerService CreateAnalyzer(string path)
+        {
+            return new PetMedsAnalyzerService(new RemoteUrlProviderService(path, Path.GetTempPath()), new ReportingService(ReportingType.PetMedsTypeCsv));
+        }
+    }
+}
---
+++ src/Core/VetDirectoryTool.Core/Service/Analyzers/PetMedsAnalyzerService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+using VetDirectoryTool.Core.Parser;
+using VetDirectoryTool.Core.Parser.Model;
+using VetDirectoryTool.Core.Service.Analyzers.Model;
+using VetDirectoryTool.Core.Service.FileProvider;
+using VetDirectoryTool.Core.Service.Reporting;
+
+namespace VetDirectoryTool.Core.Service.Analyzers
+{
+    public class PetMedsAnalyzerService
+    {
+        private readonly IFileProvider FileProvider;
+        private readonly ReportingService ReportingService;
+
+        public PetMedsAnalyzerService(IFileProvider fileProvider, ReportingService reportingService)
+        {
+            FileProvider = fileProvider;
+            ReportingService = reportingService;
+        }
+
+        public async Task AnalyzeAsync(DirectoryModel analyzeModel)
+        {
+            var content = await FileProvider.GetContentAsync();
+            var parserResponse = new ParserFactory().Analyze(new ParserRequestModel(content));
+            await ReportingService.ExportAsync(analyzeModel.OutputPath, parserResponse.ParserFileModels);
+        }
+
+    }
+}
