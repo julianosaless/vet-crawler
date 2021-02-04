f28b066ee6aca2ad839b6059bc78975896cb5cc9
Juliano Sales <juliano.salesg@gmail.com>
2021-02-04 03:12:00 -0300

    Parser: Extract structure

--- src/Core/VetDirectoryTool.Core/Parser/Template/PetMedsParserTemplate.cs
+++ src/Core/VetDirectoryTool.Core/Parser/Template/PetMedsParserTemplate.cs
@@ -1,15 +1,67 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 using VetDirectoryTool.Core.Parser.Model;
+using System.Text.RegularExpressions;
+using System.Text.Json;
 
 namespace VetDirectoryTool.Core.Parser.Template
 {
     public class PetMedsParserTemplate : IParser
     {
-        public List<ParserFileModel> Analyze(ParserRequestModel parserRequest)
+        private const string OnlyDataStoreInfo = "data-store-info=.*";
+
+        public List<ParserFileModel> Analyze(ParserRequestModel parserRequest) => Evaluate(parserRequest.Content);
+
+        private List<ParserFileModel> Evaluate(string source)
         {
-            throw new NotImplementedException();
+            var groupResults = Regex.Matches(source, OnlyDataStoreInfo);
+
+            return groupResults
+                .Where(x => x.Success)
+                .Select(x => x.Value)
+                .Select(candidate => ExtractToModel(Sanitize(candidate)))
+                .Distinct()
+                .ToList();
+        }
+
+        private ParserFileModel ExtractToModel(string value)
+        {
+            var informations = value.Split('-');
+            string ExtractName()
+            {
+                var currentInfo = informations.Single(x => x.StartsWith("name:"));
+                return currentInfo.Replace("name:", string.Empty);
+            }
+
+            string ExtractAddress()
+            {
+                var currentInfo = informations.Single(x => x.StartsWith("address1:"));
+                return currentInfo.Replace("address1:", string.Empty);
+            }
+
+            string ExtractPhone()
+            {
+                var currentInfo = informations.Single(x => x.StartsWith("ID:"));
+                return currentInfo.Replace("ID:", string.Empty);
+            }
+            return new ParserFileModel(ExtractName(), ExtractAddress(), ExtractPhone());
+        }
+
+        private string Sanitize(string value)
+        {
+            return value
+                .Replace("data-store-info", string.Empty)
+                .Replace("{", string.Empty)
+                .Replace("}", string.Empty)
+                .Replace("=", string.Empty)
+                .Replace("/>", string.Empty)
+                .Replace("\":\"", ":")
+                .Replace("\",\"", "-")
+                .Replace(",", "-")
+                .Replace("\"", string.Empty)
+                .Trim();
         }
     }
 }
---
+++ src/Core/VetDirectoryTool.Core/Parser/Template/PetMedsViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace VetDirectoryTool.Core.Parser.Template
+{
+    public class PetMedsViewModel
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+        public string address1 { get; set; }
+        public object address2 { get; set; }
+        public string city { get; set; }
+        public string postalCode { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string stateCode { get; set; }
+        public string countryCode { get; set; }
+        public double distance { get; set; }
+        public bool hideOnDirectory { get; set; }
+    }
+}
